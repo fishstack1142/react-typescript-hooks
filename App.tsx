20ff2536b8d78c6aa4beae623d345ea595b01735
fishstack1142 <fishstack1142@gmail.com>
2020-08-03 07:48:52 +0700

    Initial form and buttom

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import StateHooksComponent from './StateHooksComponent';
+
+
+const App: React.FC = () => {
+
+  return (
+    <div className="App">
+      <StateHooksComponent />
+    </div>
+  );
+
+}
+
+export default App;
